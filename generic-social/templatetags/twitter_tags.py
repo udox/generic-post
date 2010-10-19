8ec1466f3587d3ee6a1fd8f561467cbe9a5f3511
Chris Khan <chris.khan@u-dox.com>
2010-10-19 16:18:59 +0100

    Social: Fixed tweets and gathered tweet code to execute rather then seperating it (maintainability)

--- generic-social/templatetags/twitter_tags.py
+++ generic-social/templatetags/twitter_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.conf import settings
 from sitepost.models import *
+from django.utils.http import urlquote
+from django.contrib.sites.models import Site
 
 register = template.Library()   
  
@@ -8,17 +10,12 @@ register = template.Library()
 @register.inclusion_tag('tags/twitter.html')   
 def twitter(obj): 
     tweet_url = obj.get_full_url  
-    tweet_title = '%s...' % obj.title[:80]
+    tweet_title = '%s... Read more at:' % obj.title[:80]  
        
     return {
-       'tweet_url': tweet_url, 
-       'tweet_title': tweet_title,   
+       'tweet_url': tweet_url,
+       'tweet_title': tweet_title,      
     }
 
-
-@register.inclusion_tag('tags/twitter_js.html')   
-def twitter_js():     
-    return ''
-        
     
     
