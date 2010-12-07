74512f076ac102a4b07d2ce16e1e749e4401a3be
Chris Khan <chris.khan@u-dox.com>
2010-12-07 17:16:18 +0000

    sites: some comment and code cleanup

--- public/media/js/list_sites.js
+++ public/media/js/list_sites.js
@@ -3,14 +3,12 @@ $(document).ready(function(){
         e.preventDefault();
         clicked = $(this).attr('id');
         clicked = clicked.replace('show_site_', '');
-       // $('.site_list ul').hide();
         $('.site_list ul').each(function(sites){
+            $('#site_list_'+clicked).show(); 
             if ($(this).attr('id') != 'site_list_'+clicked){
-                console.log($(this).attr('id'))
                 $(this).hide();
             }         
-        });
-        $('#site_list_'+clicked).show();        
+        });               
     });
     $('.site_list ul:not(:first)').hide();
 });
